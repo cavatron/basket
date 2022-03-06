8e718c0a01efa76aea65a70a6aab4342af3a6dc9
Cavanaugh Richards <cavatron@pm.me>
2022-03-06 13:37:19 -0600

    make a basket

---
+++ src/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="page">
+    <Basket v-bind="state" />
+
+    <div class="form">
+      <div>H/W Ratio: {{ (state.height / state.width).toFixed(2) }}</div>
+
+      <div class="input">
+        <div>Width</div>
+        <input type="number" v-model="state.width" />
+      </div>
+      <div class="input">
+        <div>Height</div>
+        <input type="number" v-model="state.height" />
+      </div>
+      <div class="input">
+        <div>Gap</div>
+        <input type="number" v-model="state.gap" />
+      </div>
+      <div class="input">
+        <div>Rows</div>
+        <input type="number" v-model="state.rows" />
+      </div>
+      <div class="input">
+        <div>Columns</div>
+        <input type="number" v-model="state.columns" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { reactive } from "vue";
+import Basket from "./Basket.vue";
+
+const state = reactive({
+  width: 25,
+  height: 50,
+  rows: 2,
+  columns: 2,
+  gap: 5,
+});
+</script>
+
+<style scoped>
+.page {
+  padding: 50px;
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  height: 100%;
+}
+
+.form {
+  display: flex;
+  gap: 5px;
+  flex-direction: column;
+  margin-top: 20px;
+}
+
+.input {
+  display: flex;
+  flex-direction: row;
+  gap: 10px;
+  align-items: center;
+}
+
+.input div {
+  width: auto;
+  min-width: 60px;
+}
+</style>
---
+++ src/Basket.vue
@@ -0,0 +1,86 @@
+<script>
+import { h } from "vue";
+
+export default {
+  props: ["width", "height", "rows", "columns", "gap"],
+  render() {
+    let rects = [];
+
+    let padding = 20;
+
+    let offset = 0;
+
+    for (let row = 0; row < this.rows; row++) {
+      let left = padding;
+
+      if (row > 0) {
+        if (row % 2 === 0) {
+          offset += this.width / 2 - this.height / 2 + this.height + this.gap;
+        } else {
+          offset += this.height / 2 - this.width / 2 + this.width + this.gap;
+        }
+      }
+
+      for (let col = 0; col < this.columns; col++) {
+        let top = padding + offset;
+        let width = this.width;
+        let height = this.height;
+
+        if (row % 2 === 0) {
+          if (col % 2 === 0) {
+            width = this.height;
+            height = this.width;
+
+            top += width / 2 - height / 2;
+          }
+
+          if (col > 0) {
+            left += height + this.gap;
+          }
+        } else {
+          if (col % 2 === 0) {
+            left += height / 2 - width / 2;
+          } else {
+            width = this.height;
+            height = this.width;
+
+            top += width / 2 - height / 2;
+          }
+
+          if (col > 0) {
+            if (col % 2 === 0) {
+              left += width + this.gap;
+              left += height / 2 - width / 2;
+            } else {
+              left += height + this.gap;
+            }
+          }
+        }
+
+        rects.push(
+          h("div", {
+            class: "rect",
+            style: {
+              width: `${width}px`,
+              height: `${height}px`,
+              top: `${top}px`,
+              left: `${left}px`,
+            },
+          })
+        );
+      }
+    }
+
+    return rects;
+  },
+};
+</script>
+
+<style scoped>
+.rect {
+  background-color: #88ffef;
+  width: 50px;
+  height: 50px;
+  position: absolute;
+}
+</style>
